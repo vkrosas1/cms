e06e8d9ed4f98e39394c63784d92b01fb016c229
Vivi <viviana.rosas1999@gmail.com>
2025-04-08 11:08:08 -0700

    adding routing

--- first-app/src/app/details/details.component.ts
+++ first-app/src/app/details/details.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   template: `
-    <p>
-      details works!
-    </p>
+  <article>
+    <img class="listing-photo" [src]="housingLocation?.photo"
+      alt="Exterior photo of {{housingLocation?.name}}"/>
+    <section class="listing-description">
+      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+    </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{housingLocation?.availableUnits}}</li>
+        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+      </ul>
+    </section>
+  </article>  
   `,
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
 
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  // housingLocationId = -1;
+  // housingLocationId needed to be defined in class to be used as variable in template
+  housingService = inject(HousingService);
+  housingLocation: Housinglocation | undefined;
+
+  constructor() { 
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
 }
--- first-app/src/app/house-location/house-location.component.ts
+++ first-app/src/app/house-location/house-location.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Housinglocation } from '../housinglocation';
+import { RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-house-location',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink, 
+    RouterOutlet,
+  ],
   template: `
    <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  </section>
+  <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
   `,
   styleUrl: './house-location.component.css'
 })

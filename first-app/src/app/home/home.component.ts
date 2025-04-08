57c3bfc2ba5da59a5ba9b7724e1fb926a4cabc0f
Vivi <viviana.rosas1999@gmail.com>
2025-04-08 14:40:55 -0700

    adding search bar functionality

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -14,13 +14,13 @@ import { HousingService } from '../housing.service';
   template: `
   <section>
       <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
       <app-house-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation">
       </app-house-location>
     </section>
@@ -32,8 +32,21 @@ export class HomeComponent {
  
   housingLocationList: Housinglocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: Housinglocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
